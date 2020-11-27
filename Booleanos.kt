17e69a8584ee0e326622d220b6f0b583520ff2d2
Cstevenlopez <cstevenlopez2001@gmail.com>
2020-11-27 09:50:12 -0600

    Ejemplo de boleanos

---
+++ Booleanos.kt
@@ -0,0 +1,11 @@
+    //booleanos
+fun main()
+{
+    val x = 1
+    val y = 2
+    val z = 2
+    val esTrue = x < y && x < z
+    val otroTrue = x == y || y == z
+    
+    println("$esTrue,$otroTrue")
+}
--- Tipos.kt
+++ Tipos.kt
@@ -6,5 +6,5 @@ val double = 12.34
 val float = 12.34F
 val hexadecimal = 0xAB
 val binary = 0b01010101
-println("$int $long $double $float $hexadecimal $binary")
+println("Los datos son In:$int Log:$long double:$double float:$float hexadecimal:$hexadecimal binary:$binary")
 }
